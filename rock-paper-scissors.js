847b56a62d9be34117cf842d665edd263a166109
Jeffrey Stenger <stenger88@gmail.com>
2021-04-15 19:44:49 +0200

    Add in function to play 5 rounds and tally+display scores.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,51 +1,100 @@
-const whichWeapon = ["Rock", "Paper", "Scissors"];
+const whichWeapon = ["Rock", "Paper", "Scissors"];	
 
-function computerPlay(whichWeapon) { 		//a function that creates the computer's choice
+//a function that creates the computer's choice
+function computerPlay() { 											
 	return whichWeapon[Math.floor(Math.random() * whichWeapon.length)];
 }
 
-const computerSelection = computerPlay(whichWeapon); //declare the computer's choice
+//Adjust the user input so that first letter is capitalized and remaining letters are all lowercase.
+function titleCase(anyString){  										
+	return anyString[0].toUpperCase() + anyString.slice(1).toLowerCase();
+  }
 
-const playerPrompt = prompt("Rock, Paper, or Scissors?"); //Ask the user to enter a choice.
-
-function titleCase(playerSelection){
-  return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+//const computerSelection = computerPlay(whichWeapon); //declare the computer's choice
+function playerPlay(){
+	let playerPrompt = prompt("Rock, Paper, or Scissors?"); //Ask the user to enter a choice.
+	let playerSelection = titleCase(playerPrompt);
+	return playerSelection;
 }
-
-const playerSelection = titleCase(playerPrompt); //Adjust the user input so that first letter is capitalized and remaining letters are all lowercase.
  
 //Function to compare the computer and user's choices and return an answer basis them.
 function playRound(computerSelection, playerSelection) {
 	if (computerSelection === "Rock" && playerSelection === "Rock") {
-  		return console.log("Draw.");
+  		return "Draw.";
   	}
   	else if (computerSelection === "Rock" && playerSelection === "Paper") {
-  		return console.log("You win! Paper beats Rock.");
+  		return "You win! Paper beats Rock.";
   	}
   	else if (computerSelection === "Rock" && playerSelection === "Scissors") {
-  		return console.log("You lose! Rock beats Scissors.");
+  		return "You lose! Rock beats Scissors.";
   	}
   	else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
-	  return console.log("Draw");
+	  return "Draw";
   	}
   	else if (computerSelection === "Scissors" && playerSelection === "Paper") {
-  		return console.log("You lose! Scissors beat Paper.");
+  		return "You lose! Scissors beat Paper.";
   	}
   	else if (computerSelection === "Scissors" && playerSelection === "Rock") {
-  		return console.log("You win! Rock beats Scissors.");
+  		return "You win! Rock beats Scissors.";
   	}
     else if (computerSelection === "Paper" && playerSelection === "Paper") {
-    	return console.log("Draw");
+    	return "Draw";
     }
     else if (computerSelection === "Paper" && playerSelection === "Rock") {
-    	return console.log("You lose. Paper beats Rock");
+    	return "You lose. Paper beats Rock";
     }
     else if (computerSelection === "Paper" && playerSelection === "Scissors") {
-    	return console.log("You win! Scissors beat Paper!");
+    	return "You win! Scissors beat Paper!";
     }
     else {
-    	return console.log("Please try again and enter a valid option.");
+    	return "Please try again and enter a valid option.";
     }
 }
 
-console.log(playRound(computerSelection, playerSelection));
+let playerScore = 0;
+let computerScore = 0;
+
+//Create a function which tallies the scores for 5 rounds of play.
+function game() {
+    for (let i = 0; i < 5; i++) {  
+    let result = playRound(computerPlay(), playerPlay());
+  	if (validOption(result)) {
+	  if (!playerDraw(result) && playerWins(result)) {
+		  playerScore++;
+	  }
+	  if (!playerDraw(result) && !playerWins(result)) {
+		  computerScore++;
+	  }  
+  	}
+	if (!validOption(result)) {
+	  i--;
+	}
+	console.log(result, ` | Player:${playerScore}`,` | Computer:${computerScore}`);
+  }
+}
+
+function playerWins(result) {
+	if (result.slice(0,7) === "You win") {
+		return true;
+	}
+	else if (result.slice(0,8) === "You lose") {
+		return false;
+	}
+}
+
+function playerDraw(result) {
+	if (result.slice(0,4) === "Draw") {
+		return true;
+	}
+}
+
+function validOption(result) {
+	if (result.slice(0,2) === "Pl") {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
